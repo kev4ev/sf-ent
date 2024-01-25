fc6408a6f97607efe33c8346087f5a1f1279ada6
krice <krice@pop-os.localdomain>
2024-01-25 17:02:38 -0600

    progress

--- index.js
+++ index.js
@@ -6,6 +6,44 @@ const { Connection } = require('jsforce');
 require('dotenv').config({ override: false });
 const { SFSID, SFUNAME, SFPWD, SFURL } = process.env;
 
+const AUTH_OPTS = {
+    session: 'sessionId',
+    uname: 'username/password'
+};
+
+/**
+ * @returns {Promise<Connection>}
+ */
+async function getConnection(
+    instanceUrl=SFURL, 
+    authMethod=(SFSID ? AUTH_OPTS.session : AUTH_OPTS.uname), 
+    sessionId=SFSID, 
+    uname=SFUNAME, 
+    pwd=SFPWD)
+{
+    // connect via jsforce
+    let opts = { loginUrl: instanceUrl, instanceUrl };
+    if(authMethod === AUTH_OPTS.session){
+        opts.sessionId = sessionId;
+        opts.accessToken = sessionId;
+    }
+    const conn = new Connection(opts);
+    try{
+        if(authMethod === AUTH_OPTS.session){
+            await conn.identity(); // throws error if sessionId is invalid
+        } else{
+            await conn.loginBySoap(uname, pwd);
+        }
+    } catch(err){
+        console.error('Could not authenticate using provided auth info');
+
+        return undefined;
+    }
+
+    // return the authorized connection
+    return conn;
+}
+
 class Ent extends Command{
     /**
      * @param {Connection} connection 
@@ -19,7 +57,7 @@ class Ent extends Command{
                 const [ key, fn ] = entry;
                 this[key] = async (args) => {
                     await this.run();
-                    await this[key](this.connection, Object.assign(this.args, args));
+                    await fn(this.connection, Object.assign(this.args, args));
                 }
             }
         });
@@ -34,18 +72,18 @@ class Ent extends Command{
         }
     }
 
+    /** Establishes connection in non-interactive mode when env vars are present */
+    async preRun(){
+        if(!this?.connection?.accessToken && !this?.args?.interactive){
+            this.connection = await getConnection();
+        }
+    }
+
     async readyToExecute(){
         return this.connection && this?.connection?.accessToken ? true : 'Authorized JSForce Connection required';
     }
 
     async *getPrompts(){
-        // prompt user for auth info when missing
-        const authOpts = {
-            s: 'sessionId',
-            u: 'username/password'
-        };
-        const { s, u } = authOpts;
-
         // first prompt to get/confirm login url and auth type
         yield [
             {
@@ -57,11 +95,11 @@ class Ent extends Command{
             },
             {
                 message: 'Connect with sessionId or username/password?',
-                choices: Object.values(authOpts),
+                choices: Object.values(AUTH_OPTS),
                 type: 'select',
                 name: 'authMethod',
                 required: true,
-                initial: SFSID ? s : u
+                initial: SFSID ? AUTH_OPTS.session : AUTH_OPTS.uname
             }
         ];
 
@@ -71,31 +109,31 @@ class Ent extends Command{
             {
                 message: 'Enter session id',
                 type: 'input',
-                name: 'sid',
-                skip: authMethod === u,
-                required: authMethod === s,
+                name: 'sessionId',
+                skip: authMethod === AUTH_OPTS.uname,
+                required: authMethod === AUTH_OPTS.session,
                 initial: SFSID
             },
             {
                 message: 'Enter username',
                 type: 'input',
                 name: 'uname',
-                skip: authMethod === s,
-                required: authMethod === u,
+                skip: authMethod === AUTH_OPTS.session,
+                required: authMethod === AUTH_OPTS.uname,
                 initial: SFUNAME
             },
             {
                 message: 'Enter password',
                 type: 'password',
                 name: 'pwd',
-                skip: authMethod === s,
-                required: authMethod === u,
+                skip: authMethod === AUTH_OPTS.session,
+                required: authMethod === AUTH_OPTS.uname,
                 initial: SFPWD
             }
         ];
 
         // yield confirmation prompt to connect to sf
-        const { sid, uname, pwd, instanceUrl } = this;
+        const { sessionId, uname, pwd, instanceUrl } = this;
         yield [
             {
                 message: 'Connect now?',
@@ -105,30 +143,14 @@ class Ent extends Command{
                 initial: 'Y',
                 onSubmit: async(name, value) => {
                     if(!value) return undefined;
-                    // connect via jsforce
-                    let opts = { loginUrl: instanceUrl, instanceUrl };
-                    if(authMethod === s){
-                        opts.sessionId = sid;
-                        opts.accessToken = sid;
-                    }
-                    const conn = new Connection(opts);
-                    try{
-                        if(authMethod === s){
-                            await conn.identity(); // throws error if sessionId is invalid
-                        } else{
-                            await conn.loginBySoap(uname, pwd);
-                        }
-                    } catch(err){
-                        throw new Error('Could not authenticate using provided auth info');
-                    }
-
-                    // return the authorized connection
-                    return conn;
+                    
+                    return await getConnection(instanceUrl, authMethod, sessionId, uname, pwd);
                 }
             }
         ]
-
     }
+
+    async execute(){ /** empty for Ent */ }
 }
 
 module.exports = Ent;
