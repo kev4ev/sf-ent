2e358618726b4462d167bea2348729f774739ab0
krice <krice@pop-os.localdomain>
2024-02-14 15:46:19 -0600

    subcommand relay bug fix

--- index.test.js
+++ index.test.js
@@ -6,16 +6,19 @@ const { ent } = require('./index.js');
         await ent()
             .generate({ out: './' })
             .query({ query: 'SELECT Id FROM Case LIMIT 5' })
-            // .query('another query')
+            .query({ query: 'another query' })
             .done(); // returns root resolver
 
     debugger;
 
     // intermediate style
-    const intermediate = ent().generate({ out: './' });
-    const result1 = intermediate.query({ query: 'SELECT Id FROM Case LIMIT 5' }).done();
+    const intermediate0 = ent().generate({ out: './' }),
+        intermediate1 = intermediate0.query({ query: 'SELECT Id FROM Case LIMIT 5' }),
+        intermediate2 = intermediate1.query({ query: 'another query' }),
+        result1 = intermediate2.done();
 
-    const results = await Promise.all([ intermediate, result1 ]);
+
+    const results = await Promise.all([ intermediate0, intermediate1, intermediate2, result1 ]);
 
     debugger;
 })();
