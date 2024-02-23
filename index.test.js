5b3bbe6d6f9b673e4947623b5214256f86cdd93f
krice <krice@pop-os.localdomain>
2024-02-23 12:11:20 -0600

    working BUT intermediate3 in index.test.js is incorrect Promise

--- index.test.js
+++ index.test.js
@@ -19,11 +19,11 @@ const { ent } = require('./index.js');
     const intermediate0 = ent().generate({ out: './.generated' }),
         intermediate1 = intermediate0.query('SELECT Id FROM Case LIMIT 5'),
         intermediate2 = intermediate1.query('another query'),
-        intermediate3 = intermedate2.sobject('Account').GET('1,2,3,4,5').done(),
+        intermediate3 = intermediate2.sobject('Account').GET('1,2,3,4,5').done(),
         result1 = intermediate3.done();
 
 
-    const results = await Promise.all([ intermediate0, intermediate1, intermediate2, result1 ]);
+    const results = await Promise.all([ intermediate0, intermediate1, intermediate2, intermediate3, result1 ]);
 
     debugger;
 })();
